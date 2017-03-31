5f8757e26a756b458adfa7c6276028c3e1d5f666
Eduardo Martins <eduardo.hmartins93@gmail.com>
2017-03-31 13:58:34 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+		double nota1;
+		double nota2;
+		double nota3;
+	scanf("%lf %lf %lf",&nota1 , &nota2, &nota3);
+		double MEDIA = nota1 * 2 + nota2 * 3 + nota3 *5;
+	printf("MEDIA = %.1f\n",MEDIA /10);
+
+	return 0;
+
+}
+	
